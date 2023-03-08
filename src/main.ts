d63e050a5c9febcb39f776ded4d34c55783fd735
Jonathan Komma <j.komma@pfelling.de>
2023-03-08 20:55:39 +0100

    start of infinite mode

--- src/main.ts
+++ src/main.ts
@@ -8,46 +8,48 @@ import { Seed } from "./seed";
 
 // get CLA arguments
 const argv = yargs
-    .option('input', {
-        describe: 'Seed for start of the game',
+    .option('seed', {
+        description: 'Seed for start of the game',
         type: 'string',
-        alias: 'i',
-        conflicts: ['width', 'height', 'randomlimit']
+        alias: 's',
+        conflicts: ['width', 'height', 'randomlimit', 'infinite']
     })
     .option('width', {
-        describe: 'width of the field by default the width of the console',
+        description: 'width of the field by default the width of the console',
         type: 'number',
         alias: 'w',
-        default: process.stdout.columns,
-        
     })
     .option('height', {
-        describe: 'height of the field by default the height of the console',
+        description: 'height of the field by default the height of the console',
         type: 'number',
         alias: 'h',
-        default: process.stdout.rows - 3
     })
     .option('randomlimit', {
-        describe: 'used to regulate the spawn rate in random mode',
+        description: 'used to regulate the spawn rate in random mode. 85 by default',
         type: 'number',
         alias: 'r',
-        default: 85,
-        conflicts: 'input'
+        conflicts: ['seed', 'infinite']
+    })
+    .option('infinite', {
+        description: 'play in infinite mode',
+        type: 'boolean',
+        alias: 'i',
+        conflicts: ['seed', 'width', 'height', 'randomlimit']
     })
     .check((argv) => {
-        if (argv.height > process.stdout.rows -3) {
+        if (argv.height &&  argv.height > process.stdout.rows -3) {
             throw new Error('Passed height is too big for the terminal');
         }
 
-        if (argv.width > process.stdout.columns) {
+        if (argv.width && argv.width> process.stdout.columns) {
             throw new Error('Passed width is too big for the terminal');
         }
 
-        if (argv.height <= 0 || argv.width <= 0) {
+        if (argv.height && argv.height <= 0 || argv.width && argv.width <= 0) {
             throw new Error('Height and Width must be greater than 0');
         }
 
-        if (argv.randomlimit < 1 || argv.randomlimit > 100) {
+        if (argv.randomlimit && (argv.randomlimit < 1 || argv.randomlimit > 100)) {
             throw new Error('randomlimit must be in the range 1 - 100');
         }
     
@@ -61,15 +63,19 @@ const aliveCell: string = 'X';
 const emptyCell: string = ' ';
 
 // create a new field
-const fieldGenerator: Field = new Field(argv.height, argv.width, aliveCell, emptyCell);
+const height = argv.height || process.stdout.rows - 3;
+const width = argv.width || process.stdout.columns;
+const fieldGenerator: Field = new Field(height, width, aliveCell, emptyCell);
 
-if (argv.input) {
+if (argv.infinite) {
+    console.log('Halloooo');
+} else if (argv.seed) {
     // read seed-file and init the field
-    const seed = new Seed(argv.input);
+    const seed = new Seed(argv.seed);
     const seedCells: Cell[] = seed.cells;
     fieldGenerator.initSeed(seedCells);
 } else {
-    fieldGenerator.initRandom(argv.randomlimit);
+    fieldGenerator.initRandom(argv.randomlimit || 85);
 }
 
 // create a new game
